67175a88e2810367d0fa3764f4f5e1042f69a8a2
Tony Bowden <tony@tmtm.com>
2022-01-04 22:48:42 +0200

    compare enwiki Chief of General Staff

---
+++ sources/chief-of-staff-enwiki/scraper.rb
@@ -0,0 +1,30 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no image name start end duration].freeze
+    end
+
+    def raw_end
+      super.gsub('†', '').tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
